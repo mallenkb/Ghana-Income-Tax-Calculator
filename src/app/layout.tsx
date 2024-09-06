adca705c3f8528f4bee4d01d3b04467fa2499f53
Marlon <macbook@Marlon.local>
2024-09-06 22:16:23 +0000

    Added MS Clarity and configured Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        <Script
+          id='ms_clarity'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
+              t=l.createElement(r);t.async=1;t.src="https://clarity.microsoft.com/clarity.js";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nz5zc63bj5");
+            `
+          }}
+        >
+        </Script>
+      </body>
     </html>
   )
 }
