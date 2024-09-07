06eee43bc5d9e5d564c02d1faa1d0f09615f5672
Eugene Debrah <debraheug@gmail.com>
2024-09-07 13:37:14 +0000

    Updated UI and corrections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={`min-h-screen bg-[#F7F7F7] flex flex-col items-center p-4 ${jetbrainsMono.className}`}>
+    <main className={`min-h-screen bg-[#F7F7F7] flex flex-col items-center  sm:p-4 ${jetbrainsMono.className}`}>
       <div className="w-full max-w-[560px] mt-0 sm:mt-6">
         <Widget />
       </div>
